4baf464bdf1840a719328908822ed0f9810bed76
Dimitriev <dimitrievevgeniy@mail.ru>
2021-11-10 22:38:28 +0300

    done first

---
+++ hw_18/cmake-build-debug/CMakeFiles/hw_18.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_18.dir/main.cpp.o"
+  "hw_18"
+  "hw_18.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
