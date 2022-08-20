4aeffe9692ec003408b04bcbff4e31e1f70a80f0
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-08-20 23:33:00 +0300

    tested 22.1

--- hw_22.1/main.cpp
+++ hw_22.1/main.cpp
@@ -9,37 +9,49 @@ struct caller{
 
 int main() {
 
-    int command =0;
+    std::string command;
     caller customer;
     std::map<std::string,std::string> directory;
 
-    std::cout <<"what do you want to do?"<<std::endl;
-    std::cin>>command;
-
-    if(command==1){
-        std::cout<<"write name and number"<<std::endl;
-        std::cin>>customer.name>>customer.number;
-        directory.insert(std::pair<std::string,std::string>(customer.name,customer.number));
-    }
-    else if(command ==2){
-        std::cout <<"enter the subscriber's number to find out his name"<<std::endl;
-        std::cin>>customer.number;
-        for(std::map<std::string,std::string>::iterator it=directory.begin();it!=directory.end();++it){
-            if(it->second==customer.number){
-                std::cout<<it->first<<" ";
+    while (command!="q") {
+        std::cout <<"what do you want to do?"<<std::endl;
+        std::cin>>command;
+        if (command == "1") {
+            std::cout << "write number and name" << std::endl;
+            std::cin >> customer.number >> customer.name;
+            directory.insert(std::pair<std::string, std::string>(customer.number, customer.name));
+        } else if (command == "2") {
+            std::cout << "enter the subscriber's number to find out his name" << std::endl;
+            std::cin >> customer.number;
+            for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); ++it) {
+                if (it->first == customer.number) {
+                    std::cout << it->second << " ";
+                }
             }
+            std::cout<<std::endl;
+
+        }
+        else if (command == "3") {
+            std::cout << "enter the subscriber's name to find out his number" << std::endl;
+            std::cin >> customer.name;
+            for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); ++it) {
+                if (it->second == customer.name) {
+                    std::cout << it->first <<std::endl;
+                    break;
+                }
+            }
+
+        }
+        else if (command == "4") {
+            for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); ++it) {
+                    std::cout << it->first << " "<<it->second<<std::endl;
+t
+            }
+
         }
 
-    }
-    else if(command ==2){
-        std::cout <<"enter the subscriber's name to find out his number"<<std::endl;
-        std::cin>>customer.name;
-        for(std::map<std::string,std::string>::iterator it=directory.begin();it!=directory.end();++it){
-            if(it->first==customer.name){
-                std::cout<<it->second<<" ";
-            }
-        }
 
     }
+
 
 }
