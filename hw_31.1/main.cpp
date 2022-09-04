119d4e216d2fd0129cb8c203aa57edc3f9f29456
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-09-05 01:38:46 +0300

    31.1 need little fixes

---
+++ hw_31.1/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+class Toy{
+private:
+    std::string name;
+public:
+    Toy(std::string _name):name(_name){};
+    Toy():Toy("unknown"){};
+
+    //there may be everything, and not just simple functions and fields
+    //then I focus on the fact that there can be complex manipulations with memory
+};
+class shared_ptr_toy{
+private:
+    Toy* subject = nullptr;
+    size_t *count = new size_t(0);
+public:
+    shared_ptr_toy(){
+        subject = new Toy();
+        ++(*count);
+    };
+
+    shared_ptr_toy(std::string _name){
+        subject = new Toy(_name);
+        ++(*count);
+    };
+
+    shared_ptr_toy(shared_ptr_toy &toy){
+        subject = new Toy(*toy.subject);
+        ++(*count);
+    };
+
+    shared_ptr_toy& operator= (shared_ptr_toy &toy){
+        if(&toy != this){
+            if(*count>0){
+                subject = toy.subject;
+            }
+        }
+        return *this;
+    }
+
+    ~shared_ptr_toy(){
+        if(*count == 1){
+            delete subject;
+            delete count;
+        }
+        else {
+            *(count)--;
+        }
+
+    }
+    size_t show_count(){
+        return *count;
+    }
+
+
+
+
+
+};
+
+
+
+int main() {
+    shared_ptr_toy dog;
+    std::cout<<dog.show_count()<<std::endl;
+    shared_ptr_toy cat;
+    std::cout<<cat.show_count()<<std::endl;
+    cat = dog;
+    std::cout<<dog.show_count()<<std::endl;
+    std::cout<<cat.show_count()<<std::endl;
+
+
+
+
+
+
+
+
+}
