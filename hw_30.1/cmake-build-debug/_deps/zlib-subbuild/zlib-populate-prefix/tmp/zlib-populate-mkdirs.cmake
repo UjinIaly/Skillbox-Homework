5657323e14fa5d2f22d84d6fee003807f7818291
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-09-04 11:22:10 +0300

    30.1 in progress

---
+++ hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/tmp/zlib-populate-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-src"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-build"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/tmp"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/src/zlib-populate-stamp"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/src"
+  "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/src/zlib-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/iwaly/Skillbox-Homework/hw_30.1/cmake-build-debug/_deps/zlib-subbuild/zlib-populate-prefix/src/zlib-populate-stamp/${subDir}")
+endforeach()
---
+++ hw_30.1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main() {
+
+    std::string command ;
+    while(command!="exit"){
+        std::cout<<"enter command"<<std::endl;
+        std::cin>>command;
+        if(command == "get"){
+
+        }
+        else if(command == "post"){
+
+        }
+        else if(command == "put"){
+
+        }
+        else if(command == "delete"){
+
+        }
+        else if(command == "patch"){
+
+        }
+        else {
+            std::cout<<"error"<<std::endl;
+        }
+    }
+}
