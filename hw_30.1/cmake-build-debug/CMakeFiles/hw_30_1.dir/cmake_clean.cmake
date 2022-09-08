7fb3eb9016c03bbec703594b1c7f37816f17fa1e
iwaly <dimitrievevgeniy@mail.ru>
2022-09-09 00:17:47 +0300

    need to be delete

---
+++ hw_30.1/cmake-build-debug/CMakeFiles/hw_30_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_30_1.dir/main.cpp.obj"
+  "CMakeFiles/hw_30_1.dir/main.cpp.obj.d"
+  "hw_30_1.exe"
+  "hw_30_1.exe.manifest"
+  "hw_30_1.pdb"
+  "libhw_30_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_30_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
