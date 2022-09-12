59b2cf228816ffca629230f32625c5f93d8537d5
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-09-12 22:20:23 +0300

    done hw_30.2 on linux

--- hw_30.2.1/main.cpp
+++ hw_30.2.1/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/html"});
+
+    std::cout << r.text << std::endl;
+
+    std::cout<<std::endl;
+    size_t header_start = r.text.find("<h1>")+4;
+    size_t header_end = r.text.find("</h1>");
+    for(size_t i = header_start;i<header_end;++i ){
+        std::cout<<r.text[i];
+    }
+
 }
