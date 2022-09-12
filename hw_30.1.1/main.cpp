396bcea9d2c24896a1d3f41c22c9719007086863
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-09-12 21:41:39 +0300

    done hw_30.1 on linux

---
+++ hw_30.1.1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cpr/cpr.h>
+
+int main() {
+    std::string command;
+    while(command != "exit"){
+        std::cout<<"enter command"<<std::endl;
+        std::cin>>command;
+        if(command == "get"){
+            cpr::Response answer = cpr::Get(cpr::Url("http://httpbin.org/get"),
+                                            cpr::Parameters{{"hello", "world"}});
+            std::cout<<answer.text<<std::endl;
+        }
+        else if(command == "post"){
+            std::string phrase;
+            std::cout<<"enter phrase"<<std::endl;
+            std::cin>>phrase;
+            cpr::Response answer = cpr::Post(cpr::Url("http://httpbin.org/post"),
+                                             cpr::Payload({{"phrase: ",phrase.c_str()}}));
+            std::cout<<answer.text<<std::endl;
+        }
+        else if(command == "put"){
+            std::string phrase;
+            std::cout<<"enter phrase"<<std::endl;
+            std::cin>>phrase;
+            cpr::Response answer = cpr::Put(cpr::Url{"http://www.httpbin.org/put"},
+                                            cpr::Payload{{"key", "value"}});
+            std::cout<<answer.text<<std::endl;
+
+        }
+        else if(command == "delete"){
+            cpr::Response answer = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
+            std::cout<<answer.text<<std::endl;
+        }
+        else if(command == "patch"){
+            cpr::Response answer = cpr::Patch(cpr::Url{"http://www.httpbin.org/patch"},
+                                              cpr::Payload{{"key", "value"}});
+            std::cout<<answer.text<<std::endl;
+        }
+
+    }
+
+}
